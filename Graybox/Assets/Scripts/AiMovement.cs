78df70d04522a6d3702f3764b49784b2a3ac53d9
JBuck55 <joshuabuckmann55@gmail.com>
2023-10-18 11:09:59 -0700

    Character Controls
    Basic stuff

---
+++ Graybox/Assets/Scripts/AiMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AiMovement : MonoBehaviour
+{
+    public GameObject pointA;
+    public Transform pointB;
+    public Button startButton;
+    public Button pauseButton;
+    private Rigidbody2D rb;
+    public float speed;
+    bool isMoving = false;
+    public float jumpForce = 200;
+
+    void StopMovement()
+    {
+        Time.timeScale = 0;
+        isMoving = false;
+    }
+
+    void StartMovement()
+    {
+        Time.timeScale = 1;
+        isMoving = true; 
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpForce));
+    }
+
+    void Die()
+    {
+        transform.position = pointA.transform.position;
+        rb.velocity = new Vector2(0,0);
+        StopMovement();
+    }
+
+    void Start()
+    {
+        transform.position = pointA.transform.position;
+        Time.timeScale = 0;
+        startButton.onClick.AddListener(StartMovement);
+        pauseButton.onClick.AddListener(StopMovement);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        //testing
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+
+        //testing
+        if (Input.GetKeyDown(KeyCode.K)) Die();
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        
+    }
+}
+
+
+
+
