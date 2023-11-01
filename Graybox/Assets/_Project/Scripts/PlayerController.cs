975a67e227c229435e5602256187af8454559dcb
marshallsandovalclark <37922234+marshallsandovalclark@users.noreply.github.com>
2023-11-01 11:58:24 -0700

    Revert "Update PlayerController.cs"
    This reverts commit 156341a4f6bd26717cbe6cb97ebabf3306ca73a3.

--- Graybox/Assets/_Project/Scripts/PlayerController.cs
+++ Graybox/Assets/_Project/Scripts/PlayerController.cs
@@ -28,7 +28,6 @@ public class PlayerController : MonoBehaviour
     {
         playerAnimator.SetBool("isWalking", true);
         isWalking = true;
-        // Simulate physics again when play button is pressed.
         playerRB.simulated = true;
         playerAnimator.SetBool("isPaused", false);
         isPaused = false;
@@ -37,8 +36,6 @@ public class PlayerController : MonoBehaviour
     public void Pause()
     {
         playerAnimator.SetBool("isPaused", true);
-        // We don't want to simulate physics while the game is paused because that would make it possible for the character
-        // to keep falling when paused.
         playerRB.simulated = false;
         isPaused = true;
     }
@@ -48,10 +45,10 @@ public class PlayerController : MonoBehaviour
     {
         if (isWalking == true && isPaused == false)
         {
-            //transform.Translate(Vector3.right * playerSpeed);
 
-            // By adding a force rather than simply translating the player character's movement, we can make it possible to
-            // simulate certain surfaces such as ice or mud, as well as make movement more realistic overall.
+            //might be better to rewrite as "rb.AddForce(new Vector2(playerSpeed, 0), ForceMode2D.Impulse);"
+            //not sure if the translate method here is affected by drag and other physics attributes of the ground (might want slick ice ground or sticky mud ground). worth investigating?
+            //transform.Translate(Vector3.right * playerSpeed);
             if (playerRB.velocity.x < maxMoveSpeed)
             {
                 playerRB.AddForce(new Vector2(playerSpeed, 0), ForceMode2D.Impulse);
