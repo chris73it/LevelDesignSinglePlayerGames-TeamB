d6f0341f4b72c7048a51b544e785faba27962b87
cyo93 <59427615+cyo93@users.noreply.github.com>
2023-11-13 11:31:13 -0800

    First work on Save Manager

---
+++ Graybox/Assets/_Project/Scripts/SaveManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    int unlockedLevels = 1;
+    void Start()
+    {
+        LoadGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveGame()
+    {
+
+    }
+
+    public void LoadGame()
+    {
+
+    }
+}
