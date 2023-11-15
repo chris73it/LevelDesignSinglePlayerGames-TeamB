34c444740f6f6cc93a63bd5671361234024dc293
cyoce <masterryangp@gmail.com>
2023-11-15 11:47:11 -0800

    enemies reset now

--- Graybox/Assets/_Project/Scripts/EnemyBehaviors/GhostAI.cs
+++ Graybox/Assets/_Project/Scripts/EnemyBehaviors/GhostAI.cs
@@ -3,39 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GhostAI : MonoBehaviour
-{
+public class GhostAI : MonoBehaviour {
 
     public float speed;
     public float distance;
     public bool movingRight = true;
     private bool isActive = false;
     public Transform groundDetection;
-    
 
-    public void Play()
-    {
+
+    public void Play() {
         isActive = true;
     }
-    
-    private void Start()
-    {
+
+    private void Start() {
         PlaybackControl.play += Play;
+        PlayerController.respawn += Reset;
     }
 
 
-    private void Update()
-    {
-        if (isActive)
-        {
+    private void Update() {
+        if(isActive) {
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
         Debug.DrawRay(groundDetection.position, Vector2.down * distance, Color.red);
-        if(groundInfo.collider == null)
-        {
+        if(groundInfo.collider == null) {
             TurnAround();
         }
+    }
+
+    private void Reset() {
+        isActive = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
