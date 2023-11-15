536fe3962c83c10ccb2060b5e81b0380b6a7fdac
cyo93 <59427615+cyo93@users.noreply.github.com>
2023-11-15 11:56:29 -0800

    Added Save Manager

--- Graybox/Assets/_Project/Scripts/SaveManager.cs
+++ Graybox/Assets/_Project/Scripts/SaveManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
-    int unlockedLevels = 1;
+    public static SaveManager instance {  get; set; }
+    public int unlockedLevels = 1;
+    public bool music = true;
+    public bool sound = true;
     void Start()
     {
         LoadGame();
@@ -18,11 +21,15 @@ public class SaveManager : MonoBehaviour
 
     public void SaveGame()
     {
-
+        ES3.Save("unlockedLevels", unlockedLevels);
+        ES3.Save("sound", sound);
+        ES3.Save("music", sound);
     }
 
     public void LoadGame()
     {
-
+        unlockedLevels = ES3.Load("unlockedLevels", 1);
+        music = ES3.Load("music", true);
+        sound = ES3.Load("sound", true);
     }
 }
